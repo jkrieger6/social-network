30213946d7f614d2688437349d0348b6c4b4e3f2
jkrieger6 <jkrieger11@gmail.com>
2023-06-21 18:22:10 -0700

    added friend schema to users model

--- models/user.js
+++ models/user.js
@@ -22,10 +22,34 @@ const userSchema = new mongoose.Schema(
             max_length: 20,
             min_length: 3,
         },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [friendsSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
     }
 );
 
-const User = model('User', userSchema);
+const friendsSchema = new mongoose.Schema(
+    {
+        friendId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        friendName: String,
+    }
+);
+
+const User = mongoose.model('User', userSchema);
+
 
 module.exports = User;
 
