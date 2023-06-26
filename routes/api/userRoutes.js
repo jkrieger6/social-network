9c6ec1133cec18f69fa379a0cb97acee4ee013aa
jkrieger6 <jkrieger11@gmail.com>
2023-06-26 10:50:33 -0700

    added routes to add a new friends, and delete and new friend

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -85,4 +85,60 @@ router.delete('/:id', async (req, res) => {
     }
 });
 
+// POST (create) a new friend to a user's friend list
+router.post('/:userId/friends/:friendId', async (req, res) => {
+    try {
+        const userData = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId } },
+            { new: true }
+        );
+        if (!userData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        } else {
+            const friendData = await User.findOneAndUpdate(
+                { _id: req.params.friendId },
+                { $addToSet: { friends: req.params.userId } },
+                { new: true }
+            );
+            if (!friendData) {
+                res.status(404).json({ message: 'No friend found with this id!' });
+                return;
+            }
+        }
+        res.status(200).json({ message: 'Friend added!' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// DELETE a friend from a user's friend list
+router.delete('/:userId/friends/:friendId', async (req, res) => {
+    try {
+        const userData = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { new: true }
+        );
+        if (!userData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        } else {
+            const friendData = await User.findOneAndUpdate(
+                { _id: req.params.friendId },
+                { $pull: { friends: req.params.userId } },
+                { new: true }
+            );
+            if (!friendData) {
+                res.status(404).json({ message: 'No friend found with this id!' });
+                return;
+            }
+        }
+        res.status(200).json({ message: 'Friend deleted!' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
